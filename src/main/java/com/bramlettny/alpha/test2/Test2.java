198b7ba4a27733d0bfbd2668ca11da5e50b18dcd
John Bramlett <jbramlett@3street.net>
2019-08-09 17:47:46 -0400

    Alternative solution with single loop

--- src/main/java/com/bramlettny/alpha/test2/Test2.java
+++ src/main/java/com/bramlettny/alpha/test2/Test2.java
@@ -16,17 +16,37 @@ import java.util.List;
 
 public class Test2 {
 
+    /**
+     * Determines if there is a continuous subarray whose sum is equal to the specified target sum.
+     *
+     * Complexity:
+     *
+     *  this approach keeps a running sum of sub-arrays so as it iterates over the input data it does a second
+     *  loop over the sub-arrays. I believe this approach makes the complexity O squared. While I could reduce the
+     *  iterations of the inner array (tossing values when they are above the targetSum) the complexity of that code
+     *  doesn't feel worth it given the overall simplicity of the solution - that would be more important for larger
+     *  datasets but for smaller ones it isn't worth the cost.
+     *
+     * @param input The set of values we are checking against
+     * @param targetSum The target sum we are looking for
+     * @return boolean Returns true if we find a continuous subarray whose sum is equal to our target, false otherwise
+     */
     public static boolean findSubarryWithSum(List<Integer> input, int targetSum) {
-        int[] sums = new int[input.size()];
+        int sum = 0;
+        int start = 0;
         for (int i = 0; i < input.size(); i++) {
-            int newVal = input.get(i);
-            for (int ii = 0; ii < i; ii++) {
-                if (sums[ii] + newVal == targetSum) {
-                    return true;
-                }
-                sums[ii] += newVal;
+            sum+= input.get(i);
+            if (sum == targetSum) {
+                return true;
             }
-            sums[i] = newVal;
+
+            while (sum > targetSum && start < i) {
+                sum -= input.get(start);
+                start++;
+            }
+            if (sum == targetSum) {
+                return true;
+            }
         }
         return false;
     }
