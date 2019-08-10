150e501fce5fa6471187441797843cc68f41609f
John Bramlett <jbramlett@3street.net>
2019-08-09 20:01:11 -0400

    Adding alternative approach

--- src/main/java/com/bramlettny/alpha/test2/Test2.java
+++ src/main/java/com/bramlettny/alpha/test2/Test2.java
@@ -16,6 +16,21 @@ import java.util.List;
 
 public class Test2 {
 
+    /**
+     * Determines if there is a continuous subarray whose sum is equal to the specified target sum.
+     *
+     * Complexity:
+     *
+     *  this approach keeps a running sum of sub-arrays so as it iterates over the input data it does a second
+     *  loop over the sub-arrays. I believe this approach makes the complexity O squared. While I could reduce the
+     *  iterations of the inner array (tossing values when they are above the targetSum) the complexity of that code
+     *  doesn't feel worth it given the overall simplicity of the solution - that would be more important for larger
+     *  datasets but for smaller ones it isn't worth the cost.
+     *
+     * @param input The set of values we are checking against
+     * @param targetSum The target sum we are looking for
+     * @return boolean Returns true if we find a continuous subarray whose sum is equal to our target, false otherwise
+     */
     public static boolean findSubarryWithSum(List<Integer> input, int targetSum) {
         int[] sums = new int[input.size()];
         for (int i = 0; i < input.size(); i++) {
@@ -30,4 +45,37 @@ public class Test2 {
         }
         return false;
     }
+
+    /**
+     * Determines if there is a continuous subarray whose sum is equal to the specified target sum.
+     *
+     * Complexity:
+     * I believe the complexity of this would be considered O, it requires a single loop over the primary input.
+     * It does have a process where we access part of the main array as we "remove" values from a running total
+     * but I don't think that work changes the big-O value.
+     *
+     * @param input The set of values we are checking against
+     * @param targetSum The target sum we are looking for
+     * @return boolean Returns true if we find a continuous subarray whose sum is equal to our target, false otherwise
+     */
+    public static boolean findSubarryWithSum2(List<Integer> input, int targetSum) {
+        int sum = 0;
+        int start = 0;
+        for (int i = 0; i < input.size(); i++) {
+            sum+= input.get(i);
+            if (sum == targetSum) {
+                return true;
+            }
+
+            while (sum > targetSum && start < i) {
+                sum -= input.get(start);
+                start++;
+            }
+            if (sum == targetSum) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/test/groovy/com/bramlettny/alpha/test2/Test2Spec.groovy
+++ src/test/groovy/com/bramlettny/alpha/test2/Test2Spec.groovy
@@ -15,4 +15,17 @@ class Test2Spec extends Specification {
         [1, 2, 3, 4]    | 5     | true
         [5, 1, 4, 6]    | 10    | true
     }
+
+    def "see if there is a subarray whose sum equals a given value - 2"() {
+        expect:
+        Test2.findSubarryWithSum2(input, sum) == found
+
+        where:
+        input           | sum   | found
+        [1, 2, 3, 4]    | 7     | true
+        [5, 6, 7, 8]    | 33    | false
+        [1, 2, 3, 4]    | 5     | true
+        [5, 1, 4, 6]    | 10    | true
+    }
+
 }
